0a62a02ee3c90adae323c8a37a03a21571a016d9
princegdmweb4102-tech <princegdmweb4102@gmail.com>
2026-02-23 01:47:44 +0600

    java script intrgate

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+let interviewList = [];
+let rejectedList = []
+let currentStatus = 'all'
+
 let total = document.getElementById('total');
-let interviewCount = document.getElementById('interviewCount')
-let rejectedCount = document.getElementById('rejectedCount');
+let thrivingCount = document.getElementById('interviewCount')
+let strugglingCount = document.getElementById('rejectedCount');
+
+const allFilterBtn = document.getElementById('all-filter-btn')
+const thrivingFilterBtn = document.getElementById('interview-filter-btn')
+const strugglingFilterBtn = document.getElementById('rejected-filter-btn')
 
 const allCardSection = document.getElementById('allCards');
+const mainContainer = document.querySelector('main')
+const filterSection = document.getElementById('filtered-section')
+
 
 function calculateCount() {
     total.innerText = allCardSection.children.length 
+    thrivingCount.innerText = interviewList.length
+    strugglingCount.innerText = rejectedList.length
 }
 
 calculateCount()
+
+
