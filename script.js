c24972e3a0b7bbeebc10485fc4f6d8d99731f984
princegdmweb4102-tech <princegdmweb4102@gmail.com>
2026-02-23 02:10:47 +0600

    button color add

--- script.js
+++ script.js
@@ -23,4 +23,42 @@ function calculateCount() {
 
 calculateCount()
 
+// step 1;
+function toggleStyle(id) {
+    // adding gray bg for all
+    allFilterBtn.classList.add('bg-transparent', 'text-black')
+    interviewFilterBtn.classList.add('bg-transparent', 'text-black')
+    rejectedFilterBtn.classList.add('bg-transparent', 'text-black')
 
+    // if any button has black then remove
+    allFilterBtn.classList.remove('bg-transparent', 'text-white')
+    interviewFilterBtn.classList.remove('bg-transparent', 'text-white')
+    rejectedFilterBtn.classList.remove('bg-transparent', 'text-white')
+
+    // console.log(id);
+    const selected = document.getElementById(id)//this is the button that clicked for filter
+
+    currentStatus = id
+    console.log(currentStatus);
+    // console.log(selected);
+
+    // adding black bg for current button
+    selected.classList.remove('bg-transparent', 'text-black')
+    selected.classList.add('bg-sky-500', 'text-white')
+    // step 1 finish
+// show and hidden particular section
+    // step 4 start
+    // filtering while clicking the filter button (All, interview, rejected)
+    if (id == 'interview-filter-btn') {
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+        renderInterview()
+    } else if (id == 'all-filter-btn') {
+        allCardSection.classList.remove('hidden');
+        filterSection.classList.add('hidden')
+    } else if (id == 'rejected-filter-btn') {
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden')
+        renderRejected()
+    }
+}
