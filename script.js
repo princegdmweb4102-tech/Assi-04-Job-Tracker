20fd6be340b5eceabea5d0e09cabf6ba087466a9
princegdmweb4102-tech <princegdmweb4102@gmail.com>
2026-02-23 03:03:58 +0600

    interview and rejected add

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ let interviewList = [];
 let rejectedList = []
 let currentStatus = 'all'
 
+
+
 let total = document.getElementById('total');
-let thrivingCount = document.getElementById('interviewCount')
-let strugglingCount = document.getElementById('rejectedCount');
+let interviewCount = document.getElementById('interviewCount')
+let rejectedCount = document.getElementById('rejectedCount');
 
 const allFilterBtn = document.getElementById('all-filter-btn')
-const thrivingFilterBtn = document.getElementById('interview-filter-btn')
-const strugglingFilterBtn = document.getElementById('rejected-filter-btn')
+const interviewFilterBtn = document.getElementById('interview-filter-btn')
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn')
 
 const allCardSection = document.getElementById('allCards');
 const mainContainer = document.querySelector('main')
@@ -17,8 +19,8 @@ const filterSection = document.getElementById('filtered-section')
 
 function calculateCount() {
     total.innerText = allCardSection.children.length 
-    thrivingCount.innerText = interviewList.length
-    strugglingCount.innerText = rejectedList.length
+    interviewCount.innerText = interviewList.length
+    rejectedCount.innerText = rejectedList.length
 }
 
 calculateCount()
@@ -60,5 +62,172 @@ function toggleStyle(id) {
         allCardSection.classList.add('hidden');
         filterSection.classList.remove('hidden')
         renderRejected()
+    }
+}
+
+
+// step 2 delegation  
+mainContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('interview-btn')) {
+        const parenNode = event.target.parentNode.parentNode;
+
+        const plantName = parenNode.querySelector('.plantName').innerText
+        const light = parenNode.querySelector('.light').innerText
+        const water = parenNode.querySelector('.water').innerText
+        const status = parenNode.querySelector('.status').innerText
+        const notes = parenNode.querySelector('.notes').innerText
+
+        parenNode.querySelector('.status').innerText = 'Interview'
+
+        const cardInfo = {
+            plantName,
+            light,
+            water,
+            status: 'Interview',
+            notes
+        }
+
+        const plantExist = interviewList.find(item => item.plantName == cardInfo.plantName)
+
+        if (!plantExist) {
+            interviewList.push(cardInfo)
+        }
+
+        // step 2 finish
+        // removing the plant from rejected list
+        rejectedList = rejectedList.filter(item => item.plantName != cardInfo.plantName)
+
+        // after remove rerender the html
+        if (currentStatus == 'rejected-filter-btn') {
+            renderRejected()
+        }
+
+         calculateCount()
+
+
+    } else if (event.target.classList.contains('rejected-btn')) {
+        const parenNode = event.target.parentNode.parentNode;
+
+        const plantName = parenNode.querySelector('.plantName').innerText
+        const light = parenNode.querySelector('.light').innerText
+        const water = parenNode.querySelector('.water').innerText
+        const status = parenNode.querySelector('.status').innerText
+        const notes = parenNode.querySelector('.notes').innerText
+
+        parenNode.querySelector('.status').innerText = 'Rejected'
+
+        const cardInfo = {
+            plantName,
+            light,
+            water,
+            status: 'Rejected',
+            notes
+        }
+
+        const plantExist = rejectedList.find(item => item.plantName == cardInfo.plantName)
+
+        if (!plantExist) {
+            rejectedList.push(cardInfo)
+        }
+
+        // removing the plant from interview list
+        interviewList = interviewList.filter(item => item.plantName != cardInfo.plantName)
+
+        // console.log(interviewList);
+
+        // after remove rerender the html
+        if (currentStatus == "interview-filter-btn") {
+            renderInterview();
+        }
+        calculateCount()
+
+    }
+
+})
+
+
+
+
+
+// step 3  html file create
+function renderInterview() {
+    // make the filterSection empty every time
+    filterSection.innerHTML = ''
+
+    // crating innerHtml
+    for (let interview of interviewList) {
+        console.log(interview);
+
+        let div = document.createElement('div');
+        div.className = 'card flex justify-between border p-8'
+        div.innerHTML = `
+         <div class="space-y-6">
+                    <!-- part 1 -->
+                    <div>
+                        <p class="plantName text-4xl">${interview.plantName}</p>
+                        <p class="latinName">Latin Name</p>
+                    </div>
+
+                    <!-- part 2 -->
+                    <div class="flex gap-2">
+                        <p class="light bg-gray-200 px-5">Bright Indicate</p>
+                        <p class="water bg-gray-200 px-5">weekly</p>
+                    </div>
+                    <!-- part 3 -->
+                     <p class="status">${interview.status}</p>
+                     <p class="notes">New leaf unfurling by the east window.</p>
+
+                     <div class="flex gap-5">
+                        <button class="interview-btn bg-green-200 px-4 py-2">Interview</button>
+                        <button class="rejected-btn bg-red-200 px-4 py-2">Rejected</button>
+                     </div>
+                </div>
+
+                <!-- main part 2 -->
+                <div>
+                    <button class="btn-delete bg-red-200 text-red-600 px-4 py-2">Delete</button>
+                </div>
+        `
+        filterSection.appendChild(div)
+    }
+}
+
+function renderRejected() {
+    // make the filterSection empty every time
+    filterSection.innerHTML = ''
+    // crating innerHtml
+    for (let rejected of rejectedList) {
+
+        let div = document.createElement('div');
+        div.className = 'card flex justify-between border p-8'
+        div.innerHTML = `
+         <div class="space-y-6">
+                    <!-- part 1 -->
+                    <div>
+                        <p class="plantName text-4xl">${rejected.plantName}</p>
+                        <p class="latinName">Latin Name</p>
+                    </div>
+
+                    <!-- part 2 -->
+                    <div class="flex gap-2">
+                        <p class="light bg-gray-200 px-5">Bright Indicate</p>
+                        <p class="water bg-gray-200 px-5">weekly</p>
+                    </div>
+                    <!-- part 3 -->
+                     <p class="status">${rejected.status}</p>
+                     <p class="notes">New leaf unfurling by the east window.</p>
+
+                     <div class="flex gap-5">
+                        <button class="interview-btn bg-green-200 px-4 py-2">Interview</button>
+                        <button class="rejected-btn bg-red-200 px-4 py-2">Rejected</button>
+                     </div>
+                </div>
+
+                <!-- main part 2 -->
+                <div>
+                    <button class="btn-delete bg-red-200 text-red-600 px-4 py-2">Delete</button>
+                </div>
+        `
+        filterSection.appendChild(div)
     }
 }
